ee84ca3f5dffb6ff4fa032d62819abfa0bb09ca1
Bojie Jiang <jiangbojie@gmail.com>
2017-10-07 00:02:17 -0700

    Using Config

--- src/main/java/us/bojie/bootbo/HelloController.java
+++ src/main/java/us/bojie/bootbo/HelloController.java
@@ -1,5 +1,6 @@
 package us.bojie.bootbo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${cupSize}")
+    private String cupSize;
+
+    @Value("${age}")
+    private int age;
+
+    @Value("${content}")
+    private String content;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
-        return "Hello Bojie!";
+        return content;
     }
 }
