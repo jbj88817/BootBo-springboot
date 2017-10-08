7667685a94d50cfc1c8cdcefdc098bd0b77d1185
Bojie Jiang <jiangbojie@gmail.com>
2017-10-07 21:05:52 -0700

    Use of controller

--- src/main/java/us/bojie/bootbo/HelloController.java
+++ src/main/java/us/bojie/bootbo/HelloController.java
@@ -1,18 +1,33 @@
 package us.bojie.bootbo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     @Autowired
     private GirlProperties mGirlProperties;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = {"/say", "/hi"}, method = RequestMethod.GET)
     public String say() {
         return mGirlProperties.getCupSize();
+    }
+
+    @RequestMapping(value = "/hello/{id}", method = RequestMethod.GET)
+    public String hello(@PathVariable(value = "id") Integer id) {
+        return "id " + id;
+    }
+
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping(value = "/hello")
+    public String hello2(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id) {
+        return "id " + id;
     }
 }
